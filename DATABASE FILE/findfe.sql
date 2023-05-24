9db729210a9e0e436726dee88c4ee0777b8a11d3
Galang Prasanjaya <47128604+Rezd22@users.noreply.github.com>
2023-05-24 09:07:20 +0700

    first commit

--- DATABASE FILE/findfe.sql
+++ DATABASE FILE/findfe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2023 at 04:55 PM
+-- Generation Time: May 24, 2023 at 04:05 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,64 @@ SET time_zone = "+00:00";
 --
 -- Database: `findfe`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Sertifikat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `username`, `password`, `email`, `date`, `Sertifikat`) VALUES
+(1, 'admin', '$2y$10$mI/hpZ59vGgjs/lPTQWLJu.I82O93AEJ3gwFycAjuibOjAGi9dcTm', 'admin123@gmail.com', '2023-05-01 16:29:11', '-_180221142516-573.jpg'),
+(4, 'Developer1', '$2y$10$utdaUzlWxVuZfthHAMQWre5KtfNy3B6duWG4e9lp75SVC5FS5JNLm', 'Prasanjayagalang22@gmail.com', '2023-05-23 11:50:02', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `block_user`
+--
+
+CREATE TABLE `block_user` (
+  `blocked_from` varchar(10) NOT NULL,
+  `blocked_to` varchar(10) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `post_id`, `user_id`, `content`, `created_at`) VALUES
+(1, NULL, NULL, 'ddd', '2023-05-17 06:30:41'),
+(2, NULL, NULL, 'sss', '2023-05-23 09:16:01'),
+(3, NULL, NULL, 'sss', '2023-05-23 09:16:03');
 
 -- --------------------------------------------------------
 
@@ -43,10 +101,9 @@ CREATE TABLE `kopiesh` (
 --
 
 INSERT INTO `kopiesh` (`d_id`, `r_id`, `name`, `about`, `price`, `img`, `stock`, `date`) VALUES
-(24, 12, 'Biji Jojo', 'Biji asli Jojo', 5000, 'fakta-biji-kopi-arabika-2.jpeg', 100, '2023-05-01 16:29:05'),
+(24, 12, 'Biji Jojo', 'Biji asli Jojo', 5000, 'fakta-biji-kopi-arabika-2.jpeg', 200, '2023-05-01 16:29:05'),
 (25, 12, 'Biji Galang', 'Biji berkualitas galang', 40000, '2019_01_01T02_45_52_07_00.jpg', 30, '2023-05-01 16:29:05'),
-(26, 12, 'Biji Kopi  Emas Jojo', 'Biji Emas Berkualitas', 6000, 'Blog_Ini-Dia-Perbedaan-Kopi-Robusta-dan-Arabica-Pecinta-Kopi-Wajib-Tahu.jpg', 0, '2023-05-01 16:29:05'),
-(27, 12, 'Biji Kopi Zuama', 'Biji Berkualitas Zuama', 2000, 'images.jpeg', 100, '2023-05-01 16:31:41');
+(26, 12, 'Biji Kopi  Emas Jojo', 'Biji Emas Berkualitas', 6000, 'Blog_Ini-Dia-Perbedaan-Kopi-Robusta-dan-Arabica-Pecinta-Kopi-Wajib-Tahu.jpg', 0, '2023-05-01 16:29:05');
 
 -- --------------------------------------------------------
 
@@ -68,9 +125,78 @@ CREATE TABLE `mitra` (
 --
 
 INSERT INTO `mitra` (`mitra_id`, `username`, `password`, `email`, `date`, `Sertifikat`) VALUES
-(1, 'admin', '$2y$10$mI/hpZ59vGgjs/lPTQWLJu.I82O93AEJ3gwFycAjuibOjAGi9dcTm', 'admin123@gmail.com', '2023-04-29 15:00:01', 'p1.jpg'),
 (2, 'Mitra1', '$2y$10$H8VfsBb/W.1X4bx8qkqqHeD395i1ng73DPxG6nO41DUZu33CvEjtS', 'Mitra1@gmail.com', '2023-04-27 16:47:04', NULL),
-(3, 'Galang', '$2y$10$/dFy7TiC5CJoRgNX6QYst.1nZnyiciyr9cG2naKDSeFWPlLVWFqzu', 'Alvito@gmail.com', '2023-04-28 14:58:27', NULL);
+(3, 'Galang', '$2y$10$/dFy7TiC5CJoRgNX6QYst.1nZnyiciyr9cG2naKDSeFWPlLVWFqzu', 'Alvito@gmail.com', '2023-04-28 14:58:27', NULL),
+(4, 'mitra', '$2y$10$t37dWlwroqjYt6g388ZpQOiTv8m6grXlalTFBujVvLcyqc/J/ttlG', 'Prasanjayagalang22@gmail.com', '2023-05-23 12:10:43', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `premium`
+--
+
+CREATE TABLE `premium` (
+  `p_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `o_hr` varchar(255) NOT NULL,
+  `c_hr` varchar(255) NOT NULL,
+  `o_days` varchar(255) NOT NULL,
+  `img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `premium`
+--
+
+INSERT INTO `premium` (`p_id`, `name`, `email`, `url`, `o_hr`, `c_hr`, `o_days`, `img`) VALUES
+(0, 'Premium 10 hari', 'Prasanjayagalang22@gmail.com', 'https://www.coursehero.com/file/90786462/192410102020-Karenina-Shelfa-Mairanideva-SO-A-UTSdocx/', '6am', '5pm', 'mon-wed', 'man2.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `rating_id` int(11) NOT NULL,
+  `r_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`rating_id`, `r_id`, `rating`) VALUES
+(28, 12, 5),
+(29, 11, 3),
+(30, 10, 5),
+(31, 11, 5),
+(32, 11, 1),
+(33, 11, 3),
+(34, 11, 5),
+(35, 11, 5),
+(36, 11, 5),
+(37, 11, 5),
+(38, 12, 3),
+(39, 11, 2),
+(40, 11, 1),
+(41, 12, 5),
+(42, 10, 1),
+(43, 11, 4),
+(44, 11, 5),
+(45, 11, 5),
+(46, 11, 5),
+(47, 11, 5),
+(48, 11, 5),
+(49, 11, 5),
+(50, 11, 3),
+(51, 11, 1),
+(52, 12, 5),
+(53, 10, 5),
+(54, 10, 5);
 
 -- --------------------------------------------------------
 
@@ -88,7 +214,8 @@ CREATE TABLE `res_category` (
 --
 
 INSERT INTO `res_category` (`c_id`, `c_name`) VALUES
-(8, 'Kopi');
+(8, 'Kopi original'),
+(9, 'Kopi Premium');
 
 -- --------------------------------------------------------
 
@@ -122,6 +249,36 @@ INSERT INTO `toko` (`r_id`, `c_id`, `name`, `email`, `phone`, `url`, `o_hr`, `c_
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(20) NOT NULL,
+  `unique_id` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `user_fname` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `user_lname` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `user_email` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `bio` varchar(100) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `created_at` varchar(20) NOT NULL,
+  `dob` varchar(20) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `user_pass` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `user_avtar` varchar(200) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `user_status` varchar(10) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `last_logout` varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `unique_id`, `user_fname`, `user_lname`, `user_email`, `bio`, `created_at`, `dob`, `phone`, `address`, `user_pass`, `user_avtar`, `user_status`, `last_logout`) VALUES
+(1, '2bc812', 'Sample', 'Account', 'sample@gmail.com', 'Just a dummy account', '20-6-2021', '12-12-1980', '7410000000', '69 Dummy Address', 'password', '9b1a8f8c.jpg', 'deactive', '6/20/2021, 10:53:44 PM');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -133,16 +290,31 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `address` text NOT NULL
+  `address` text NOT NULL,
+  `picture_url` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`u_id`, `username`, `f_name`, `l_name`, `email`, `phone`, `password`, `address`) VALUES
-(35, 'Galang', 'Galang', 'Prasanjaya', 'Prasanjayagalang22@gmail.com', '0812347088', '$2y$10$FSMjd3is2w8CiRhdicPrY.1MtFmF47iYjdp.kO/isHcwYR5azuKy6', 'Jl. Riau G. 1001 No. 105 Jember'),
-(36, 'User12345', 'User', 'User', 'Prasanjayagalang22@gmail.com', '0812347088', '$2y$10$udlbio.C7klkd8Rkk67LV.v..FkcIlj/kiXgrIBBXUZgHvs6Rclu6', 'Jl. Riau G. 1001 No. 105 Jember');
+INSERT INTO `users` (`u_id`, `username`, `f_name`, `l_name`, `email`, `phone`, `password`, `address`, `picture_url`) VALUES
+(35, 'Galang', 'Galangp', 'Prasanjaya', 'Prasanjayagalang22@gmail.com', '0812347088', '$2y$10$FSMjd3is2w8CiRhdicPrY.1MtFmF47iYjdp.kO/isHcwYR5azuKy6', 'Jl. Riau G. 1001 No. 105 Jember', ''),
+(36, 'User12345', 'User', 'User', 'Prasanjayagalang22@gmail.com', '0812347088', '$2y$10$udlbio.C7klkd8Rkk67LV.v..FkcIlj/kiXgrIBBXUZgHvs6Rclu6', 'Jl. Riau G. 1001 No. 105 Jember', ''),
+(37, 'Jojo1234', 'Jojo', 'Jojo', 'Prasanjayagalang22@gmail.com', '0812347088', '$2y$10$X/uYpBZNVGN44TFoKnaXO.bQjNcH.uKHyR.UOtW6crk.4kM7NggL6', 'Jl. Riau G. 1001 No. 105 Jember', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_messages`
+--
+
+CREATE TABLE `user_messages` (
+  `time` datetime(6) NOT NULL,
+  `sender_message_id` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `receiver_message_id` varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `message` varchar(500) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 -- --------------------------------------------------------
 
@@ -170,11 +342,24 @@ CREATE TABLE `user_orders` (
 INSERT INTO `user_orders` (`o_id`, `u_id`, `d_id`, `d_name`, `quantity`, `price`, `status`, `date`, `success-date`, `r_id`) VALUES
 (35, 35, 23, 'Biji Zuama', 1, 4, NULL, '2023-04-30 22:34:39', '2023-04-30 15:34:39', 12),
 (36, 35, 24, 'Biji Jojo', 1, 1, 'rejected', '2023-04-30 22:34:39', '2023-04-30 20:39:18', 12),
-(37, 35, 25, 'Biji Galang', 1, 4, 'closed', '2023-04-30 22:34:39', '2023-04-30 20:39:10', 12);
+(37, 35, 25, 'Biji Galang', 1, 4, 'closed', '2023-04-30 22:34:39', '2023-04-30 20:39:10', 12),
+(38, 35, 25, 'Biji Galang', 1, 40000, 'closed', '2023-05-03 03:44:54', '2023-05-23 19:55:27', 12);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `kopiesh`
@@ -189,6 +374,18 @@ ALTER TABLE `mitra`
   ADD PRIMARY KEY (`mitra_id`);
 
 --
+-- Indexes for table `premium`
+--
+ALTER TABLE `premium`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `rating`
+--
+ALTER TABLE `rating`
+  ADD PRIMARY KEY (`rating_id`);
+
+--
 -- Indexes for table `res_category`
 --
 ALTER TABLE `res_category`
@@ -199,6 +396,12 @@ ALTER TABLE `res_category`
 --
 ALTER TABLE `toko`
   ADD PRIMARY KEY (`r_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`unique_id`);
 
 --
 -- Indexes for table `users`
@@ -217,6 +420,18 @@ ALTER TABLE `user_orders`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `kopiesh`
 --
 ALTER TABLE `kopiesh`
@@ -226,13 +441,19 @@ ALTER TABLE `kopiesh`
 -- AUTO_INCREMENT for table `mitra`
 --
 ALTER TABLE `mitra`
-  MODIFY `mitra_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `mitra_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `rating`
+--
+ALTER TABLE `rating`
+  MODIFY `rating_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `res_category`
 --
 ALTER TABLE `res_category`
-  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `toko`
@@ -244,13 +465,13 @@ ALTER TABLE `toko`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `user_orders`
 --
 ALTER TABLE `user_orders`
-  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `o_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
